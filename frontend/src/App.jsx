8004da1cbab4023224c1255517bd96bf26827a7a
Ashutosh Gupta <guptaashutosh502@gmail.com>
2025-05-03 11:13:05 +0530

    created folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import React from "react";
+import Home from "./pages/Home";
 
 const App = () => {
-  return (
-    <div>
-      <h1 className=" text-amber-300">Hello</h1>
-    </div>
-  );
+  return <Home />;
 };
 
 export default App;
